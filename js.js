4258d20956f4b1ffe9288a8364ed8a39a7b0d19f
Vsevolod <4193883@gmail.com>
2022-02-02 14:21:24 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,8 @@
+a = parseInt(prompt('first number:'))
+b = parseInt(prompt('second number:'))
+result = []
+result.push(a+b)
+result.push(a-b)
+result.push(a/b)
+result.push(a*b)
+console.log(result)
